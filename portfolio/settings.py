d87a3e90b9924d24e9bb0ce81eaa0b31c86709c2
jborjon <work@josephborjon.com>
2021-02-23 23:48:08 -0700

    Abstract away secret key

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .private import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wra7mw)lfj(9jw*+*uk5e#0mv*((*wvuz)oz@j%q9lzk*sm&e6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
